5bdd0402ad771fc1e83c86ed48c0bc784e469564
Andre Sjuve <andre.sjuve@gmail.com>
2021-10-01 17:31:55 +0200

    updated README

---
+++ R/utils.R
@@ -0,0 +1,102 @@
+
+# find_file() ------------------------------------------------------------------
+
+#' Extract file path from folder
+#'
+#' \code{find_file} is a function that takes the name of a file and returns
+#' the path to the file, if found.
+#'
+#' Used in scripts: {}
+#' Robust function: TRUE
+#'
+#' @param name Character vector of the name of the file to look for
+#'
+#' @param ext The file extension *without* a period (.)
+#'
+#' @param file_num Numeric. In case more than one file matches the \code{name}
+#' argument, one can specify which of the files to return. Defaults to one.
+#'
+#' @return Returns the file path, if the file is not found it returns an error.
+#'         If more than one file matches the name, a list of all matches is
+#'         also printed to console to facilitate picking the right file_num
+#'
+#' @export
+
+find_file <- function(name, ext = "fst", file_num = 1) {
+
+  if (missing(name)) stop("File name missing", call. = F)
+
+  path <- fs::path_filter(
+    fs::dir_ls(
+      type    = "file",
+      recurse = T,
+      glob    = paste0("*.", ext)
+    ),
+    regexp = name)
+
+  lpath <- length(path)
+
+  if (lpath == 0) stop("File not found", call. = F)
+
+  if (lpath > 1) {
+
+    out_path <- path[file_num]
+
+    cat(
+      glue::glue_col(
+        "{red Warning:}
+    More than one file matched the name {name}.
+    Returning match nr {file_num}: {out_path}"), sep = "\n")
+
+    cat(
+      crayon::bold(
+        crayon::silver("All matches:")
+      ),
+      crayon::cyan(path),
+      sep = "\n")
+
+    out_path
+
+  } else {
+    path
+  }
+}
+
+
+# tidymess() -------------------------------------------------------------------
+
+#' Long user messages in a tidy way
+#'
+#' \code{tidymess} let's the programmer write longer and more informative
+#' messages to the user by ensuring that the message is printed in a tidy way
+#' to the console. One can specify which type of message to give: error,
+#' warning or message.
+#'
+#' Used in scripts: {}
+#' Robust function: TRUE
+#'
+#' @param ... Message to user
+#'
+#' @param type Character, either "error", "warning" or "message"
+#'
+#' @param prefix Prefix before message
+#'
+#' @param initial Initial, ""
+#'
+#' @return The input message either in form of an error, warning or message.
+#'  If the input type does not match either, an error is thrown.
+#'
+#' @export
+
+tidymess <- function(..., type = "warning", prefix = " ", initial = ""){
+
+  type <- match.arg(type, c("error", "warning", "message"))
+
+  if (type == "error") {
+    stop(strwrap(..., prefix = prefix, initial = initial), call. = F)
+  } else if (type == "warning") {
+    warning(strwrap(..., prefix = prefix, initial = initial), call. = F)
+  } else{
+    message(strwrap(..., prefix = prefix, initial = initial), call. = F)
+  }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# flowaid
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+[![CRAN
+status](https://www.r-pkg.org/badges/version/flowaid)](https://CRAN.R-project.org/package=flowaid)
+<!-- badges: end -->
+
+The goal of flowaid is to help ease the workflow in R projects by having
+functions that finds project files etc.
+
+## Installation
+
+You can install the Github version of flowaid from
+[GitHub](https://github.com/AndreSjuve/texaid) with:
+
+``` r
+devtools::install_github("AndreSjuve/flowaid")
+```
